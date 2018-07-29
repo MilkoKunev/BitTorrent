5892e6da22345000eec1272f4c0641d17f7c2a69
Milko Kunev <milko.kunev@gmail.com>
2018-07-29 18:13:46 +0300

    Initial commit

---
+++ lib/bittorrent.ex
@@ -0,0 +1,18 @@
+defmodule Bittorrent do
+  @moduledoc """
+  Documentation for Bittorrent.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bittorrent.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
