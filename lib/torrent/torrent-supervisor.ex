c057faef727ef01671d4168d67fcb26ca64415c5
Milko Kunev <milko.kunev@gmail.com>
2018-08-21 11:19:27 +0300

    Create torrent supervisor
    * It will start everything connected to a torrent - swarm, file handler and piece storage

--- lib/torrent/torrent-supervisor.ex
+++ lib/torrent/torrent-supervisor.ex
@@ -1,20 +1,58 @@
 defmodule BitTorrent.Torrent.Sup do
   use Supervisor
 
+  alias BitTorrent.Utils
+
   def start_link(args) do
-    Supervisor.start_link(__MODULE__, args)
+    Supervisor.start_link(__MODULE__, args[:torrent])
   end
 
-  def init(args) do
-    peers = BitTorrent.Peer.Controller.get_peers(args.info_hash, args.announce_url)
+  def init(torrent_info) do
+    client_id = Application.get_env(:bittorrent, :client_id)
+    port = Application.get_env(:bittorrent, :port)
+    block_length = Application.get_env(:bittorrent, :block_length)
 
-  end
+    IO.inspect client_id
 
-  defp create_children_specs([], acc) do
-    acc
-  end
+    announce = Map.get(torrent_info, "announce")
+    info = Map.get(torrent_info, "info")
+    info_hash = Map.get(torrent_info, "info_hash")
+    piece_length = Map.get(info, "piece length")
+    length = Map.get(info, "length")
+    pieces = Map.get(info, "pieces")
+    name = Map.get(info, "name")
 
-  defp create_children_specs([peer_info | tail], other_info, acc) do
+    peers =
+      BitTorrent.Discovery.Controller.get_peers(info_hash, announce)
+      |> Enum.filter(fn peer ->
+          Map.get(peer, "peer id") != client_id
+      end)
 
+    torrent = Torrent.new(info_hash, piece_length, length, pieces, name, block_length, peers)
+
+    swarm_id = Utils.generate_id()
+
+    bitfield_size = torrent.bitfield_length
+    bitfield_name = Utils.generate_id()
+    bitfield = BitFieldSet.new!(<<0::size(bitfield_size)>>, bitfield_size)
+
+    # TODO: Think of a better way for updating servers' names
+    torrent = %{torrent | bitfield_name: bitfield_name}
+
+    children = [
+      %{
+        id: bitfield_name,
+        start: {BitTorrent.Torrent.File.BitField, :start_link, [[bitfield: bitfield, name: bitfield_name]]},
+        type: :worker
+      },
+      %{
+        id: swarm_id,
+        start: {BitTorrent.Swarm.Sup, :start_link, [[torrent: torrent]]},
+        restart: :temporary,
+        type: :supervisor
+      }
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
