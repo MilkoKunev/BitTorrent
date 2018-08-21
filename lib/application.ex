b5298ca29f7c5b6147551e8a6c8b49f631cf26f6
Milko Kunev <milko.kunev@gmail.com>
2018-08-21 11:23:13 +0300

    Update application behaviour

--- lib/application.ex
+++ lib/application.ex
@@ -4,9 +4,10 @@ defmodule Bittorrent.Application do
 
   def start(_type, _args) do
     children = [
-      {Task.Supervisor, name: BitTorrent.TaskSupervisor},
-      {BitTorrent.Peer.Sup, [name: Peer.Supervisor]},
-      {DynamicSupervisor, name: BitTorrent.Torrents.Sup}
+      {Task.Supervisor, [name: BitTorrent.TaskSupervisor, type: :supervisor]},
+      {BitTorrent.Discovery.Sup, [name: Discovery.Supervisor, type: :supervisor]},
+      {DynamicSupervisor, [strategy: :one_for_one, name: BitTorrent.Torrents.Sup, type: :supervisor]},
+      {BitTorrent.Connection.Handler, []}
     ]
 
     opts = [strategy: :one_for_one, name: Bittorrent.Supervisor]
